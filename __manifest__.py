7bc8e04e731aebf77fda373b16d9e761ab457a3c
onlyone-odoo <matiasb@onlyone.com.ar>
2025-04-03 12:08:45 -0300

    [UPD][english only][pos_webhook_sender]

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,13 @@
-# pylint: disable=missing-module-docstring,pointless-statement
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "POS Webhook Sender",
-    "summary": "Envía datos de órdenes POS a un webhook si el cliente tiene número móvil.",
+    "summary": "Sends POS order data to a webhook if the customer has a mobile number.",
     "author": "Be OnlyOne",
     "maintainers": ["onlyone-odoo"],
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Sales/Point of Sale",
-    "version": "16.0.1.0.0",
+    "version": "16.0.2.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
